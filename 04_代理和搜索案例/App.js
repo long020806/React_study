167e58de428ec4bdb1025851baee57589774c845
long020806 <cstslswust@163.com>
2021-09-01 10:53:33 +0800

    05ready

---
+++ 04_代理和搜索案例/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import List from './components/List'
+import Search from './components/Search'
+export default class App extends Component {
+
+    state={users:[],
+            isFirst:true,
+            isLoading:false,
+            err:"",
+    }
+    updateAppState = (stateObj)=>{
+        this.setState(stateObj);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Search updateAppState={this.updateAppState}/>
+                <List {...this.state} />
+            </div>
+        )
+    }
+}
