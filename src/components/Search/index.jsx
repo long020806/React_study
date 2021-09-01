88725441abad757b8b7239db120fc523c48fa814
long020806 <cstslswust@163.com>
2021-09-01 14:14:03 +0800

    fetch使用

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,11 +3,13 @@ import PubSub from 'pubsub-js'
 import axios from 'axios'
 export default class Search extends Component {
 
-    search = ()=>{
+    search = async ()=>{
         const {keyWordEle:{
             value:keyWord
         }} = this;
         // this.props.updateAppState({isFirst:false,isLoading:true})
+        //#region 发送请求使用axios
+        /*
         PubSub.publish('atguigu',{isFirst:false,isLoading:true});
         axios.get(`/api1/search/users?q=${keyWord}`).then(
             res=>{
@@ -19,7 +21,20 @@ export default class Search extends Component {
                 // this.props.updateAppState({isLoading:false,err:err.message})
                  PubSub.publish('atguigu',{isLoading:false,err:err.message});
             }
-        )
+        )*/
+        //#endregion
+
+        //#region 使用fetch发送请求 
+        PubSub.publish('atguigu',{isFirst:false,isLoading:true});
+        try{
+            const response = await fetch(`/api1/search/users2?q=${keyWord}`);
+            const data = await  response.json();
+            PubSub.publish('atguigu',{users:data.items,isLoading:false});
+
+        }catch(err){
+            PubSub.publish('atguigu',{isLoading:false,err:err.message});
+        }
+        //#endregion
     }
     render() {
         return (
