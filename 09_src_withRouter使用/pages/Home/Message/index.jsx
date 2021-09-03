cc872a99c3c1aca884311e2ccf7d51eac2e16abe
long020806 <cstslswust@163.com>
2021-09-03 13:59:35 +0800

    路由完成

---
+++ 09_src_withRouter使用/pages/Home/Message/index.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail';
+
+export default class Message extends Component {
+    state = {
+        messageArr:[
+            {id:'01',title:"消息1"},
+            {id:'02',title:"消息2"},
+            {id:'03',title:"消息3"},
+        ]
+    }
+    render() {
+        const {messageArr} = this.state;
+        return (
+            <div>
+                <ul>
+                    {
+                        messageArr.map(item=>{
+                            return (
+                                <li key={item.id}>
+                                    {/* 向路由组件传递params参数 */}
+                                    <Link to={`/home/message/detail/${item.id}/${item.title}`}>{item.title}</Link>&nbsp;&nbsp;
+                                    {/* 向路由组件传递search参数 */}
+                                    {/* <Link to={`/home/message/detail?id=${item.id}&title=${item.title}`}>{item.title}</Link>&nbsp;&nbsp; */}
+                                    {/* 向路由组件传递state参数 */}
+                                    {/* <Link to={{pathname:'/home/message/detail',state:{id:item.id,title:item.title}}}>{item.title}</Link>&nbsp;&nbsp; */}
+
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <hr />
+                <Switch>
+                    {/* 声明接收params参数 */}
+                    <Route path="/home/message/detail/:id/:title" component={Detail}></Route>
+                    {/* search参数无需声明接收 */}
+                    {/* <Route path="/home/message/detail" component={Detail}></Route> */}
+                    {/* state参数无需声明接收 */}
+                    {/* <Route path="/home/message/detail" component={Detail}></Route> */}
+                </Switch>
+            </div>
+        )
+    }
+}
