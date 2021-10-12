2b86c8d6c36fa0c5a740c8709474bb3d3ff66ead
long020806 <cstslswust@163.com>
2021-10-12 09:48:59 +0800

    react-redux实现完成

--- src/lib/react-redux/index.js
+++ src/lib/react-redux/index.js
@@ -4,7 +4,6 @@
  */
 import React, { Component } from 'react'
 import PropTypes from "prop-types"
-import {dispatch} from "../redux/index"
 export default class Provider extends Component {
     static propTypes = {
         store:PropTypes.object.isRequired,
@@ -31,26 +30,48 @@ export const connect = (mapStateToProps,mapDispatchToProps)=>{
             static contextTypes = {
                 store:PropTypes.object,
             }
-
-            render(){
-                const store = this.context.store;
-                const state = mapStateToProps(store)||{};
-                let dispatchFunc;
-                if(typeof mapStateToProps === "object"){
-                    dispatchFunc = Object.keys(mapStateToProps).reduce((pre,key)=>{
-                        pre[key] = function(){
-                            let args = arguments;
-                            return dispatch(mapStateToProps[key](arguments));
-                        };
+            constructor(props,context){
+                super(props);
+                console.log(context);
+                const {store} = context;
+                const stateProps = mapStateToProps(store.getState())
+                let dispatchProps;
+                if(typeof mapDispatchToProps==='function')
+                    dispatchProps = mapDispatchToProps(store.dispatch)
+                else if( typeof mapDispatchToProps==='object'){
+                    dispatchProps = Object.keys(mapDispatchToProps).reduce((pre,key)=>{
+                        const actionCreator = mapDispatchToProps[key];
+                        pre[key] = (...args)=> store.dispatch(actionCreator(...args));//参数透传
                         return pre;
-                    },{})
-                }else if(typeof mapStateToProps ==='function'){
-                    dispatchFunc = mapStateToProps(dispatch);
-                }else{
-                    dispatchFunc = {};
+                    },{});
                 }
+
+                this.dispatchProps = dispatchProps;
+                this.state = {...stateProps}
+                store.subscribe(()=>{//store内部状态数据发生改变
+                    //更新UI组件状态
+                    this.setState({...mapStateToProps(store.getState())})
+                })
+            }
+            render(){
+                // const store = this.context.store;
+                // const state = mapStateToProps(store.getState())||{};
+                // let dispatchFunc;
+                // if(typeof mapStateToProps === "object"){
+                //     dispatchFunc = Object.keys(mapStateToProps).reduce((pre,key)=>{
+                //         pre[key] = function(){//yes
+                //             let args = arguments;
+                //             return dispatch(mapStateToProps[key](arguments));
+                //         };
+                //         return pre;
+                //     },{})
+                // }else if(typeof mapStateToProps ==='function'){
+                //     dispatchFunc = mapStateToProps(dispatch);
+                // }else{
+                //     dispatchFunc = {};
+                // }
                 return (
-                    <UIComponent {...state} {...disptachFunc}/>
+                    <UIComponent {...this.state} {...this.dispatchProps}/>
                 )
             }
         }
